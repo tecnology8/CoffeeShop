940ab3fe385524bfbe9ab369e8677eebb2ece480
Ruddy Enmanuel Cruceta Mateo <ruddycruceta@hotmail.com>
2023-07-27 23:24:47 -0400

    Add project files.

---
+++ CoffeeShop/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CoffeeShop;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("fontello.ttf", "Icons");
+				fonts.AddFont("Rubik-Regular.ttf", "RubikRegular");
+				fonts.AddFont("Rubik-Light.ttf", "RubikLight");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
